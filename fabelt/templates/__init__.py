cd3e272b60f6f5dce146196335f8b6fb6020f940
Victor Torres <vpaivatorres@gmail.com>
2017-10-21 16:55:51 -0300

    introduce jinja2 templates and nginx.conf file

---
+++ fabelt/templates/__init__.py
@@ -0,0 +1,16 @@
+import jinja2
+import os
+
+
+__all__ = ['render']
+
+
+def render(filename, args=None):
+    if not args:
+        args = dict()
+
+    path = os.path.dirname(__file__)
+    loader = jinja2.FileSystemLoader(path)
+    env = jinja2.Environment(loader=loader)
+    tmpl = env.get_template(filename)
+    return tmpl.render(args)
