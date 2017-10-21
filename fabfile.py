1fa70ddeceb0cc95493558429044dcff6770a27b
Victor Torres <vpaivatorres@gmail.com>
2017-10-21 16:15:02 -0300

    create sample setup method

--- fabfile.py
+++ fabfile.py
@@ -1,1 +1,22 @@
+from fabric.api import env, task
+
 from fabelt import *
+
+
+env.project = 'my-project'
+env.project_home = '/var/sites/{project}'.format(project=env.project)
+env.virtualenv = '/usr/local/pythonenv/{project}'.format(project=env.project)
+
+
+@task
+def setup():
+    """Example setup task"""
+    pip.setup()
+    virtualenv.install()
+    virtualenv.create(env.virtualenv)
+    with cd(env.project_home):
+        with virtualenv.activate(env.virtualenv):
+            pip.install_requirements()
+            django.migrate()
+            django.createsuperuser()
+            django.collectstatic()
