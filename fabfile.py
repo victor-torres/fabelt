47600ebb877c89f7a7befe346bda22450a1033b0
Victor Torres <vpaivatorres@gmail.com>
2017-10-21 16:27:37 -0300

    improving example fabfile

--- fabfile.py
+++ fabfile.py
@@ -11,6 +11,16 @@ env.virtualenv = '/usr/local/pythonenv/{project}'.format(project=env.project)
 @task
 def setup():
     """Example setup task"""
+    # Required packages
+    apt.install('nginx')
+    apt.install('uwsgi')
+    apt.install('postgresql')
+
+    # Development packages
+    apt.install('build-essential')
+    apt.install('python-dev')
+    apt.install('libpq-dev')
+
     pip.setup()
     virtualenv.install()
     virtualenv.create(env.virtualenv)
