a0c7086f7904cbc4eec87bdf4513ce18e82f851b
evangilo <evangilo@gmail.com>
2017-10-31 13:35:20 -0300

    adding the create_database and drop_database functions to postgres module #7

--- fabelt/postgres.py
+++ fabelt/postgres.py
@@ -1,4 +1,4 @@
-from fabric.api import task, run
+from fabric.api import cd, sudo, task
 
 from fabelt import apt
 
@@ -7,3 +7,20 @@ from fabelt import apt
 def install():
     apt.install('postgresql')
     apt.install('libpq-dev')
+
+
+@task()
+def create_database(name, owner=None):
+    if owner:
+        cmd = 'createdb {name} --owner {owner}'.format(name=name, owner=owner)
+    else:
+        cmd = 'createdb {name}'.format(name=name)
+
+    with cd('~postgres'):
+        sudo(cmd, user='postgres')
+
+
+@task()
+def drop_database(name):
+    with cd('~postgres'):
+        sudo('dropdb {name}'.format(name=name), user='postgres')
